56726fde9dc1409425dd6a1c7d013c763c7cf945
Mohammed Yahaya <muhsinbnyahya@gmail.com>
2024-03-23 07:09:02 -0700

    Handles the user' State

---
+++ models/state.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""State Module: Inherits from Superclass BaseModel"""
+
+from models.base_model import BaseModel
+
+class State(BaseModel):
+    """State class that inherits from BaseModel."""
+    name: str = ""
