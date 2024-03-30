541600add08a8314c9f136f0a16dca5a00dbdba6
Mohammed Yahaya <muhsinbnyahya@gmail.com>
2024-03-29 17:22:47 -0700

    code

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
-""" File Storage class
+"""File Storage class
 for serialization into a JSON file and
 deserialization of JSON file
-into an instances."""
-
+into an instances.
+"""
 
 import json
 from os import path
@@ -17,18 +17,22 @@ from models.place import Place
 from models.review import Review
 
 
-class Filestorage:
-    """
-    A class that uses the following  private class attributes:
-    __file_path: string - path to the JSON file (ex: file.json)
-    __objects: dictionary - empty but will store all
-    objects by <class name>.id
-    (ex: to store a BaseModel object with id=12121212, the key will be
-    BaseModel.12121212)"""
+class FileStorage:
 
-    classes = {
-            "BaseModel": BaseModel, "User": User, "Place": Place, "State":
-            State, "City": City, "Amenity": Amenity, "Review": Review
+    """
+        Private class attributes:
+    __file_path: string - path to the JSON file
+    __objects: dictionary - empty but will store all objects by <class name>.id
+    """
+
+    CLASSES = {
+        'BaseModel': BaseModel,
+        'User': User,
+        'State': State,
+        'City': City,
+        'Amenity': Amenity,
+        'Place': Place,
+        'Review': Review
     }
 
     __file_path = "file.json"  # path to the JSON file
